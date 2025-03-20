11e97ef5e609b118b8aff3cc648517e09808156a
rohxn-rai <rohan.rai932@outlook.com>
2025-03-20 10:17:42 -0400

    modified header - added nav bar component

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,17 +1,20 @@
 import Link from "next/link";
 import { Button } from "./ui/button";
 
+import Nav from "@/components/Nav";
+
 const Header = () => {
   return (
     <header className="py-8 xl:py-12 text-white">
-      <div className="container max-auto">
+      <div className="container xl:mx-auto mx-5">
         {/* lgoo */}
         <Link href="/">
           <h1 className="text-4xl font-semibold">
-            Rohan Rai<span className="text-accent">.</span>
+            Rohan Rai<span className="text-purple-500">.</span>
           </h1>
         </Link>
         {/* destop navigation */}
+        <Nav />
       </div>
     </header>
   );
