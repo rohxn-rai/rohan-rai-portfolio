d8b091f9bf0d1db5526a68003dce087ea1b38410
rohxn-rai <rohan.rai932@outlook.com>
2025-03-24 07:59:35 -0400

    modified Home - aligned the css to match the Header

--- app/page.jsx
+++ app/page.jsx
@@ -1,3 +1,12 @@
-export default function Home() {
-  return <main>homepage</main>;
-}
+import { Button } from "@/components/ui/button";
+import { FiDownload } from "react-icons/fi";
+
+const Home = () => {
+  return (
+    <section className="h-full">
+      <div className="container xl:mx-auto lg:mx-auto px-5">homepage</div>
+    </section>
+  );
+};
+
+export default Home;
