47cc0eb0f060776880d8894c71c42212bf94e8a1
rohxn-rai <rohan.rai932@outlook.com>
2025-03-24 04:19:06 -0400

    modified nav - added proper navigations to home, services, work, resume and contact

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,5 +1,37 @@
+"use client";
+
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+const links = [
+  { name: "home", path: "/" },
+  { name: "services", path: "/services" },
+  { name: "resume", path: "/resume" },
+  { name: "work", path: "/work" },
+  { name: "contact", path: "/contact" },
+];
+
 const Nav = () => {
-  return <nav>desktop nav</nav>;
+  const pathname = usePathname();
+
+  return (
+    <nav className="flex gap-8">
+      {links.map((link, index) => {
+        return (
+          <Link
+            href={link.path}
+            key={index}
+            className={`${
+              link.path === pathname &&
+              "text-purple-500 border-b-2 border-purple-500"
+            }`}
+          >
+            {link.name}
+          </Link>
+        );
+      })}
+    </nav>
+  );
 };
 
 export default Nav;
