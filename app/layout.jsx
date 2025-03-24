ad840dd7bb0647e46c8ff17d573cc68d23463628
rohxn-rai <rohan.rai932@outlook.com>
2025-03-24 04:46:54 -0400

    modified layout - added pagetransition

--- app/layout.jsx
+++ app/layout.jsx
@@ -2,6 +2,7 @@ import { JetBrains_Mono } from "next/font/google";
 import "./globals.css";
 
 import Header from "@/components/Header";
+import PageTransition from "@/components/PageTransition";
 
 const jetbrainsMono = JetBrains_Mono({
   subsets: ["latin"],
@@ -19,7 +20,8 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={jetbrainsMono.variable}>
         <Header />
-        {children}
+
+        <PageTransition>{children}</PageTransition>
       </body>
     </html>
   );
