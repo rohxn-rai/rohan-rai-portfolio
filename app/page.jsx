e524d0d40e3dde315f250ad0d624e19b3afb887f
rohxn-rai <rohan.rai932@outlook.com>
2025-03-24 09:38:25 -0400

    modified global.css - removed redundent code from css

--- app/page.jsx
+++ app/page.jsx
@@ -4,7 +4,24 @@ import { FiDownload } from "react-icons/fi";
 const Home = () => {
   return (
     <section className="h-full">
-      <div className="container xl:mx-auto lg:mx-auto px-5">homepage</div>
+      <div className="container xl:mx-auto lg:mx-auto px-5 h-full">
+        <div className="flex flex-col xl:flex-row lg:flex-row items-center justify-between xl:pt-8 xl:pb-24 lg:pt-8 lg:pb-24">
+          {/* text */}
+
+          <div>
+            <span>Software Developer</span>
+            <h1 className="text-[48px] xl:text-[80px] lg:text-[80px] leading-[1.1] font-semibold">
+              Hello I'm
+              <br />
+              <span>Rohan Rai</span>
+            </h1>
+          </div>
+
+          {/* photo */}
+
+          <div>photo</div>
+        </div>
+      </div>
     </section>
   );
 };
