37d290ddd71774305bd0047ff32b7e88584fadec
rohxn-rai <rohan.rai932@outlook.com>
2025-03-25 04:23:20 -0400

    modified Nav - removed the static data and replaced it with navLink.json

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -3,13 +3,7 @@
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 
-const links = [
-  { name: "home", path: "/" },
-  { name: "services", path: "/services" },
-  { name: "resume", path: "/resume" },
-  { name: "work", path: "/work" },
-  { name: "contact", path: "/contact" },
-];
+import { links } from "@/data/navLinks";
 
 const Nav = () => {
   const pathname = usePathname();
