6ad25e3649ac49a644a0f8dcb45fb88f318abce7
rohxn-rai <rohan.rai932@outlook.com>
2025-03-25 04:56:09 -0400

    modified Social - mapped all the icons with the repective socials

--- components/Social.jsx
+++ components/Social.jsx
@@ -1,16 +1,27 @@
 import Link from "next/link";
 
-import { FaGithub, FaLinkedinIn, FaYoutube, FaTwitter } from "react-icons/fa";
+import { FaGithub, FaLinkedinIn, FaYoutube } from "react-icons/fa";
+import { FaXTwitter } from "react-icons/fa6";
 
 const socials = [
   { icon: <FaGithub />, path: "" },
   { icon: <FaLinkedinIn />, path: "" },
-  { icon: <FaYoutube />, path: "" },
-  { icon: <FaTwitter />, path: "" },
+  // { icon: <FaYoutube />, path: "" },
+  { icon: <FaXTwitter />, path: "" },
 ];
 
-const Socials = () => {
-  return <div className=""></div>;
+const Socials = ({ containerStyles, iconStyles }) => {
+  return (
+    <div className={containerStyles}>
+      {socials.map((item, index) => {
+        return (
+          <Link key={index} href={item.path} className={iconStyles}>
+            {item.icon}
+          </Link>
+        );
+      })}
+    </div>
+  );
 };
 
 export default Socials;
