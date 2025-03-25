884c43ffec24483003d8438b8250d06fbaf00d3d
rohxn-rai <rohan.rai932@outlook.com>
2025-03-25 11:19:09 -0400

    modified Social - added links to linkedin, github and x

--- components/Social.jsx
+++ components/Social.jsx
@@ -3,10 +3,10 @@ import Link from "next/link";
 import { FaGithub, FaLinkedinIn, FaYoutube, FaXTwitter } from "react-icons/fa6";
 
 const socials = [
-  { icon: <FaGithub />, path: "" },
-  { icon: <FaLinkedinIn />, path: "" },
+  { icon: <FaGithub />, path: "https://github.com/rohxn-rai" },
+  { icon: <FaLinkedinIn />, path: "https://www.linkedin.com/in/rohxnrai/" },
   // { icon: <FaYoutube />, path: "" },
-  { icon: <FaXTwitter />, path: "" },
+  { icon: <FaXTwitter />, path: "https://www.x.com/rohxnrai" },
 ];
 
 const Socials = ({ containerStyles, iconStyles }) => {
