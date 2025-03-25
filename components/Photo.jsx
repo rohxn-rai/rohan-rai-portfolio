78c136bd9142b61ca312a22e98b85cdaf14689c9
rohxn-rai <rohan.rai932@outlook.com>
2025-03-25 06:50:06 -0400

    modified Photo - added responsive photo element that blends with the background - no actual profile picture yet, using a dummy one

--- components/Photo.jsx
+++ components/Photo.jsx
@@ -1,5 +1,38 @@
+"use client";
+
+import { motion } from "framer-motion";
+import Image from "next/image";
+
 const Photo = () => {
-  return <div>Photo</div>;
+  return (
+    <div className="w-full h-full relative">
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{
+          opacity: 1,
+          transition: { delay: 1, duration: 0.25, ease: "easeIn" },
+        }}
+      >
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{
+            opacity: 1,
+            transition: { delay: 1.25, duration: 0.25, ease: "easeIn" },
+          }}
+          className="w-[298px] h-[298px] lg:h-[498px] lg:w-[498px] mix-blend-lighten"
+        >
+          <Image
+            src="/assets/photo.png"
+            priority
+            quality={100}
+            fill
+            alt=""
+            className="object-contain"
+          />
+        </motion.div>
+      </motion.div>
+    </div>
+  );
 };
 
 export default Photo;
