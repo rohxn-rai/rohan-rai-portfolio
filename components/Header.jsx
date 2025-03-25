c9eda7d5397f2d42e61a779eb5824fb47c097ef0
rohxn-rai <rohan.rai932@outlook.com>
2025-03-25 03:39:24 -0400

    modified header - removed the xl tailwind

--- components/Header.jsx
+++ components/Header.jsx
@@ -18,7 +18,7 @@ const Header = () => {
 
         {/* destop navigation */}
 
-        <div className="hidden xl:flex lg:flex items-center gap-8">
+        <div className="hidden lg:flex items-center gap-8">
           <Nav />
           <Link href="/contact">
             <Button>Hire me</Button>
@@ -27,7 +27,7 @@ const Header = () => {
 
         {/* mobile navigation */}
 
-        <div className="xl:hidden lg:hidden">
+        <div className="lg:hidden">
           <MobileNav />
         </div>
       </div>
