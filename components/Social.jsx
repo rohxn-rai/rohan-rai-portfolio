c6b99d177ba134722c656de126ec31beb455487a
rohxn-rai <rohan.rai932@outlook.com>
2025-03-25 04:12:19 -0400

    added Social - a component which contains all the socials icons and links

---
+++ components/Social.jsx
@@ -0,0 +1,16 @@
+import Link from "next/link";
+
+import { FaGithub, FaLinkedinIn, FaYoutube, FaTwitter } from "react-icons/fa";
+
+const socials = [
+  { icon: <FaGithub />, path: "" },
+  { icon: <FaLinkedinIn />, path: "" },
+  { icon: <FaYoutube />, path: "" },
+  { icon: <FaTwitter />, path: "" },
+];
+
+const Socials = () => {
+  return <div className=""></div>;
+};
+
+export default Socials;
