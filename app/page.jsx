dc5f02f8dee0ce5eb93bd1b00ffed980a712aee9
rohxn-rai <rohan.rai932@outlook.com>
2025-03-26 10:09:25 -0400

    modified Home - added Stats component

--- app/page.jsx
+++ app/page.jsx
@@ -3,6 +3,7 @@ import { FiDownload } from "react-icons/fi";
 
 import Social from "@/components/Social";
 import Photo from "@/components/Photo";
+import Stats from "@/components/Stats";
 
 const Home = () => {
   return (
@@ -57,6 +58,7 @@ const Home = () => {
           </div>
         </div>
       </div>
+      <Stats />
     </section>
   );
 };
