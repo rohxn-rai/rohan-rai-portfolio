034aa8676aa0ae9946e21a9f09c386aead864147
rohxn-rai <rohan.rai932@outlook.com>
2025-03-26 13:06:54 -0400

    modified Stats - changes the breakpoint to lg instead of xl

--- components/Stats.jsx
+++ components/Stats.jsx
@@ -25,18 +25,18 @@ const Stats = () => {
   return (
     <section>
       <div className="container mx-auto">
-        <div className="flex flex-wrap gap-6 max-w-[80vw] mx-auto lg:max-w-none">
+        <div className="flex flex-wrap gap-6 max-w-[80vw] mx-auto xl:max-w-none">
           {stats.map((item, index) => {
             return (
               <div
-                className="flex-1 flex gap-4 items-center justify-center lg:justify-start"
+                className="flex-1 flex gap-4 items-center justify-center xl:justify-start"
                 key={index}
               >
                 <CountUp
                   end={item.num}
                   duration={5}
                   delay={1.5}
-                  className="text-4xl lg:text-6xl font-extrabold"
+                  className="text-4xl xl:text-6xl font-extrabold"
                 />
                 <p>{item.text}</p>
               </div>
