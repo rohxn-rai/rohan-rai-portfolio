9fb417b29d6fa4f5e868f6737db5e8282afb797d
rohxn-rai <rohan.rai932@outlook.com>
2025-03-26 13:03:05 -0400

    modified Stats - added count up for stats

--- components/Stats.jsx
+++ components/Stats.jsx
@@ -1,7 +1,51 @@
 "use client";
 
+import CountUp from "react-countup";
+
+const stats = [
+  {
+    num: 1,
+    text: "Years of experience",
+  },
+  {
+    num: 4,
+    text: "Projects completed",
+  },
+  {
+    num: 10,
+    text: "Skillset mastered",
+  },
+  {
+    num: 500,
+    text: "Code commits",
+  },
+];
+
 const Stats = () => {
-  return <div>Stats</div>;
+  return (
+    <section>
+      <div className="container mx-auto">
+        <div className="flex flex-wrap gap-6 max-w-[80vw] mx-auto lg:max-w-none">
+          {stats.map((item, index) => {
+            return (
+              <div
+                className="flex-1 flex gap-4 items-center justify-center lg:justify-start"
+                key={index}
+              >
+                <CountUp
+                  end={item.num}
+                  duration={5}
+                  delay={1.5}
+                  className="text-4xl lg:text-6xl font-extrabold"
+                />
+                <p>{item.text}</p>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Stats;
