b7a8d254893c1874039fb743ad85f5fa8400fd2a
rohxn-rai <rohan.rai932@outlook.com>
2025-03-30 10:27:49 -0400

    modified Resume - replaced all the tabs content with their respective components

--- app/resume/page.jsx
+++ app/resume/page.jsx
@@ -8,9 +8,8 @@ import {
   FaReact,
   FaNodeJs,
   FaGitAlt,
-  FaGraduationCap,
-  FaBriefcase,
 } from "react-icons/fa6";
+
 import { VscVscode } from "react-icons/vsc";
 import {
   SiTypescript,
@@ -49,66 +48,6 @@ const about = {
     {
       fieldName: "Languages",
       FieldValue: "Englsih, Hindi",
-    },
-  ],
-};
-
-// experience data
-
-const experience = {
-  icons: <FaBriefcase />,
-  title: "Experience",
-  description: "I am a MERN stack developer at Digi-Safari LLC.",
-  info: [
-    {
-      company: "Digi-Safari LLC",
-      position: "MERN Stack Developer",
-      duration: "Dec 2024 - Present",
-    },
-    {
-      company: "AI LifeBOT",
-      position: "Data Science Intern",
-      duration: "Apr 2024 - Jul 2024",
-    },
-    {
-      company: "RadicalX",
-      position: "AI Engineer Intern",
-      duration: "Nov 2023 - Feb 2024",
-    },
-  ],
-};
-
-//  education data
-
-const education = {
-  icon: <FaGraduationCap />,
-  title: "Education",
-  description:
-    "I am a 2024 graduate from New Horizon College of Engineering with Bachelor of Engineering in Artificial Intelligence and Machine Learning.",
-  info: [
-    {
-      institution: "Meritshot",
-      major: "Data Science and Machine Learning",
-      duration: "Sep 2024 - Present",
-    },
-    {
-      institution: "New Horizon College of Engineering",
-      degree: "Bachelor of Engineering",
-      major: "Artificial Intelligence and Machine Learning",
-      duration: "Sep 2020 - Oct 2024",
-    },
-    {
-      institution: "G D Goenka Public School",
-      degree: "CBSE Board",
-      major: "XII",
-      minor: "PCM with Computer Science",
-      duration: "Mar 2019 - Mar 2020",
-    },
-    {
-      institution: "G D Goenka Public School",
-      degree: "CBSE Board",
-      major: "X",
-      duration: "Mar 2017 - Mar 2018",
     },
   ],
 };
@@ -184,9 +123,12 @@ import {
   TooltipTrigger,
 } from "@/components/ui/tooltip";
 
-import { ScrollArea } from "@/components/ui/scroll-area";
-
 import { motion } from "framer-motion";
+
+import Experience from "@/components/Experience";
+import Education from "@/components/Education";
+import Skills from "@/components/Skills";
+import AboutMe from "@/components/AboutMe";
 
 const Resume = () => {
   return (
@@ -215,9 +157,19 @@ const Resume = () => {
           <div className="min-h-[70vh] w-full">
             {/* experience */}
 
-            <TabsContent value="experience" className="w-full">
-              experience
-            </TabsContent>
+            <Experience />
+
+            {/* education */}
+
+            <Education />
+
+            {/* skills */}
+
+            <Skills />
+
+            {/* about */}
+
+            <AboutMe />
           </div>
         </Tabs>
       </div>
