f12809d807339e8690fcbb178c9b51fd3c6fea0f
rohxn-rai <rohan.rai932@outlook.com>
2025-03-31 05:52:42 -0400

    modified AboutMe - made changes to the width of description

--- components/AboutMe.jsx
+++ components/AboutMe.jsx
@@ -7,30 +7,30 @@ import { ScrollArea } from "@/components/ui/scroll-area";
 const about = {
   title: "About Me",
   description: "",
-  info: [
+  item: [
     {
       fieldName: "Name",
-      FieldValue: "Rohan Rai",
+      fieldValue: "Rohan Rai",
     },
     {
       fieldName: "Phone",
-      FieldValue: "(+91) 971 750 3708",
+      fieldValue: "(+91) 971 750 3708",
     },
     {
       fieldName: "Email",
-      FieldValue: "rohan.rai932@gmail.com",
+      fieldValue: "rohan.rai932@gmail.com",
     },
     {
       fieldName: "Experience",
-      FieldValue: "1 Year",
+      fieldValue: "1 Year",
     },
     {
       fieldName: "Nationality",
-      FieldValue: "Indian",
+      fieldValue: "Indian",
     },
     {
       fieldName: "Languages",
-      FieldValue: "Englsih, Hindi",
+      fieldValue: "Englsih, Hindi",
     },
   ],
 };
@@ -38,7 +38,25 @@ const about = {
 const AboutMe = () => {
   return (
     <TabsContent value="about" className="w-full">
-      about
+      <div className="flex flex-col gap-[30px] text-center xl:text-left">
+        <h3 className="text-4xl font-bold">{about.title}</h3>
+        <p className="w-full max-w-[1200px] text-white/60 mx-auto xl:mx-0">
+          {about.description}
+        </p>
+        <ul className="grid grid-cols-1 lg:grid-cols-2 gap-y-6 gap-x-16 max-w-[750px] mx-auto xl:mx-0">
+          {about.item.map((item, index) => {
+            return (
+              <li
+                key={index}
+                className="flex items-center justify-center xl:justify-start gap-4"
+              >
+                <span className="text-white/60">{item.fieldName}</span>
+                <span className="text-xl">{item.fieldValue}</span>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
     </TabsContent>
   );
 };
