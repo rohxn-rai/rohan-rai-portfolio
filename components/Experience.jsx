09c57beeaf11cf1785f5ce76aba39d0ec7c868ef
rohxn-rai <rohan.rai932@outlook.com>
2025-03-31 05:53:19 -0400

    modified Experience - made changes to the width of description

--- components/Experience.jsx
+++ components/Experience.jsx
@@ -37,7 +37,7 @@ const Experience = () => {
     <TabsContent value="experience" className="w-full">
       <div className="flex flex-col gap-[30px] text-center xl:text-left">
         <h3 className="text-4xl font-bold">{experience.title}</h3>
-        <p className="max-w-[600px] text-white/60 mx-auto xl:mx-0">
+        <p className="w-full max-w-[1200px] text-white/60 mx-auto xl:mx-0">
           {experience.description}
         </p>
         <ScrollArea className="h-[600px]">
