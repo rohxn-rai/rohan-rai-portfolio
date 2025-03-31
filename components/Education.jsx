96edfd5a74e54f2e3678368d0302ddb50d695e6b
rohxn-rai <rohan.rai932@outlook.com>
2025-03-31 05:53:08 -0400

    modified Education - made changes to the width of description

--- components/Education.jsx
+++ components/Education.jsx
@@ -44,7 +44,7 @@ const Education = () => {
     <TabsContent value="education" className="w-full">
       <div className="flex flex-col gap-[30px] text-center xl:text-left">
         <h3 className="text-4xl font-bold">{education.title}</h3>
-        <p className="max-w-[600px] text-white/60 mx-auto xl:mx-0">
+        <p className="w-full max-w-[1200px] text-white/60 mx-auto xl:mx-0">
           {education.description}
         </p>
         <ScrollArea className="h-[600px]">
