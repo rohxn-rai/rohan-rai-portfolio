edb5eb68f66e436aa0e16088073dabe5ce42efdc
rohxn-rai <rohan.rai932@outlook.com>
2025-03-31 04:22:42 -0400

    modified AboutMe - added my details

--- components/AboutMe.jsx
+++ components/AboutMe.jsx
@@ -2,6 +2,39 @@ import { TabsContent } from "./ui/tabs";
 
 import { ScrollArea } from "@/components/ui/scroll-area";
 
+// about data
+
+const about = {
+  title: "About Me",
+  description: "",
+  info: [
+    {
+      fieldName: "Name",
+      FieldValue: "Rohan Rai",
+    },
+    {
+      fieldName: "Phone",
+      FieldValue: "(+91) 971 750 3708",
+    },
+    {
+      fieldName: "Email",
+      FieldValue: "rohan.rai932@gmail.com",
+    },
+    {
+      fieldName: "Experience",
+      FieldValue: "1 Year",
+    },
+    {
+      fieldName: "Nationality",
+      FieldValue: "Indian",
+    },
+    {
+      fieldName: "Languages",
+      FieldValue: "Englsih, Hindi",
+    },
+  ],
+};
+
 const AboutMe = () => {
   return (
     <TabsContent value="about" className="w-full">
