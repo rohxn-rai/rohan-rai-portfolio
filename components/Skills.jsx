d0f6707ea4297a1a46ea6ea39e375b8799ae8cc2
rohxn-rai <rohan.rai932@outlook.com>
2025-03-31 04:43:19 -0400

    modified Skills - added ScrollArea

--- components/Skills.jsx
+++ components/Skills.jsx
@@ -99,26 +99,28 @@ const Skills = () => {
             {skills.description}
           </p>
         </div>
-        <ul className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-4 xl:gap-[30px]">
-          {skills.skillset.map((skill, index) => {
-            return (
-              <li key={index}>
-                <TooltipProvider delayDuration={100}>
-                  <Tooltip>
-                    <TooltipTrigger className="w-full h-[150px] bg-[#232329] rounded-xl flex justify-center items-center group">
-                      <div className="text-6xl group-hover:text-purple-700 transition-all duration-300">
-                        {skill.icon}
-                      </div>
-                    </TooltipTrigger>
-                    <TooltipContent>
-                      <p>{skill.name}</p>
-                    </TooltipContent>
-                  </Tooltip>
-                </TooltipProvider>
-              </li>
-            );
-          })}
-        </ul>
+        <ScrollArea className="h-[650px]">
+          <ul className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-4 xl:gap-[30px]">
+            {skills.skillset.map((skill, index) => {
+              return (
+                <li key={index}>
+                  <TooltipProvider delayDuration={100}>
+                    <Tooltip>
+                      <TooltipTrigger className="w-full h-[150px] bg-[#232329] rounded-xl flex justify-center items-center group">
+                        <div className="text-6xl group-hover:text-purple-700 transition-all duration-300">
+                          {skill.icon}
+                        </div>
+                      </TooltipTrigger>
+                      <TooltipContent>
+                        <p>{skill.name}</p>
+                      </TooltipContent>
+                    </Tooltip>
+                  </TooltipProvider>
+                </li>
+              );
+            })}
+          </ul>
+        </ScrollArea>
       </div>
     </TabsContent>
   );
