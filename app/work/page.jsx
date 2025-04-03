661ddee79d1f682637d056f23e5afa845dbe4f1b
rohxn-rai <rohan.rai932@outlook.com>
2025-04-03 03:31:24 -0400

    modified Work - increase the delay

--- app/work/page.jsx
+++ app/work/page.jsx
@@ -7,6 +7,7 @@ import { motion } from "framer-motion";
 import { useState } from "react";
 
 import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
 
 import { BsArrowUpRight, BsGithub } from "react-icons/bs";
 
@@ -23,15 +24,13 @@ const projects = [
     category: "Machine Learning",
     title: "Reflair - Reddit Flair Prediction",
     description:
-      "This is a Project for prediction of flairs in each subreddit submissions.",
+      "This is a Project for prediction of flairs in each subreddit submissions. It used PRAW and the Reddit API to fetch data from reddit and then passes the data to the Model for prediction.",
     stack: [
-      { name: "HTML" },
-      { name: "CSS" },
-      { name: "Python" },
+      { name: "Py" },
       { name: "Flask" },
       { name: "JavaScript" },
-      { name: "Data Science" },
-      { name: "Machine Learning" },
+      { name: "DS" },
+      { name: "ML" },
     ],
     image: "/work/Reflair/test1.jpg",
     live: "",
@@ -44,14 +43,28 @@ const projects = [
     description:
       "This is a Project weather focast prediction. This project is able to predict the weather forcast of the next 7 days with an accuracy of 98% and the next 14 days with am accuracy of 94%.",
     stack: [
-      { name: "HTML" },
-      { name: "CSS" },
-      { name: "Python" },
-      { name: "Machine Learning" },
+      { name: "Py" },
+      { name: "ML" },
       { name: "ReactJS" },
       { name: "MongoDB" },
     ],
     image: "/work/Reflair/test2.jpg",
+    live: "",
+    github: "",
+  },
+  {
+    num: "03",
+    category: "Machine Learning",
+    title: "Reflair - Reddit Flair Prediction",
+    description:
+      "This is a Project weather focast prediction. This project is able to predict the weather forcast of the next 7 days with an accuracy of 98% and the next 14 days with am accuracy of 94%.",
+    stack: [
+      { name: "Py" },
+      { name: "ML" },
+      { name: "ReactJS" },
+      { name: "MongoDB" },
+    ],
+    image: "/work/Reflair/test3.jpg",
     live: "",
     github: "",
   },
@@ -60,7 +73,130 @@ const projects = [
 const Work = () => {
   const [project, setProject] = useState(projects[0]);
 
-  return <div>work page</div>;
+  const handleSlideChange = (swiper) => {
+    const currentIndex = swiper.realIndex;
+
+    setProject(projects[currentIndex]);
+  };
+
+  return (
+    <motion.section
+      initial={{ opacity: 0 }}
+      animate={{
+        opacity: 1,
+        transition: { delay: 1.1, duration: 0.25, ease: "easeIn" },
+      }}
+      className="min-h-[80vh] flex flex-col justify-center py-12 xl:px-0"
+    >
+      <div className="container mx-auto px-5">
+        <div className="flex flex-col xl:flex-row xl:gap-[30px]">
+          <div className="w-full xl:w-[50%] xl:h-[460px] flex flex-col xl:justify-between order-2 xl:order-none">
+            <div className="flex flex-col gap-[30px] h-[50%]">
+              {/* outline Num */}
+
+              <div className="text-8xl leading-none font-extrabold text-transparent [-webkit-text-stroke:1px_#fff] group-hover:[-webkit-text-stroke:1px_#7e22ce] transition-all duration-500">
+                {project.num}
+              </div>
+
+              {/* project category */}
+
+              <h2 className="text-[42px] font-bold leading-none text-white group-hover:text-purple-700 transition-all duration-500 capitalize">
+                {project.category} project
+              </h2>
+
+              {/* project description */}
+
+              <p className="text-white/60">{project.description}</p>
+
+              {/* stack */}
+
+              <ul className="flex gap-4">
+                {project.stack.map((item, index) => {
+                  return (
+                    <li key={index} className="text-xl text-purple-700">
+                      {item.name}
+                      {index !== project.stack.length - 1 && ","}
+                    </li>
+                  );
+                })}
+              </ul>
+
+              {/* border */}
+
+              <div className="border border-white/20" />
+
+              {/* button */}
+
+              <div className="flex items-center gap-4">
+                {/* live project */}
+
+                <Link href={project.live}>
+                  <TooltipProvider delayDuration={100}>
+                    <Tooltip>
+                      <TooltipTrigger className="w-[70px] h-[70px] rounded-full bg-white/5 flex justify-center items-center group">
+                        <BsArrowUpRight className="text-white text-3xl group-hover:text-purple-700" />
+                      </TooltipTrigger>
+                      <TooltipContent>
+                        <p>Github Repository</p>
+                      </TooltipContent>
+                    </Tooltip>
+                  </TooltipProvider>
+                </Link>
+
+                {/* github */}
+
+                <Link href={project.github}>
+                  <TooltipProvider delayDuration={100}>
+                    <Tooltip>
+                      <TooltipTrigger className="w-[70px] h-[70px] rounded-full bg-white/5 flex justify-center items-center group">
+                        <BsGithub className="text-white text-3xl group-hover:text-purple-700" />
+                      </TooltipTrigger>
+                      <TooltipContent>
+                        <p>Live Project</p>
+                      </TooltipContent>
+                    </Tooltip>
+                  </TooltipProvider>
+                </Link>
+              </div>
+            </div>
+          </div>
+          <div className="w-full xl:w-[50%]">
+            <Swiper
+              loop={true}
+              spaceBetween={30}
+              slidesPerView={1}
+              className="xl:h-[520px] mb-12"
+              onSlideChange={handleSlideChange}
+            >
+              {projects.map((project, index) => {
+                return (
+                  <SwiperSlide key={index} className="w-full">
+                    <div className="h-[460px] relative group flex justify-center items-center bg-purple-50/20">
+                      {/* overlay */}
+
+                      <div></div>
+
+                      {/* image */}
+
+                      <div className="relative w-full h-full">
+                        <Image
+                          src={project.image}
+                          fill
+                          quality={100}
+                          className=" object-cover"
+                          alt={project.title}
+                        />
+                      </div>
+                    </div>
+                  </SwiperSlide>
+                );
+              })}
+            </Swiper>
+          </div>
+        </div>
+      </div>
+    </motion.section>
+  );
 };
 
 export default Work;
