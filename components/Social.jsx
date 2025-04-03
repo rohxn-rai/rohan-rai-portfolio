5daf9580c56384451ca894f084f8ef634b4fd4ba
rohxn-rai <rohan.rai932@outlook.com>
2025-04-03 06:46:33 -0400

    modified Socials - edited GitHub link

--- components/Social.jsx
+++ components/Social.jsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 import { FaGithub, FaLinkedinIn, FaYoutube, FaXTwitter } from "react-icons/fa6";
 
 const socials = [
-  { icon: <FaGithub />, path: "https://github.com/rohxn-rai" },
+  { icon: <FaGithub />, path: "https://www.github.com/rohxn-rai" },
   { icon: <FaLinkedinIn />, path: "https://www.linkedin.com/in/rohxnrai/" },
   // { icon: <FaYoutube />, path: "" },
   { icon: <FaXTwitter />, path: "https://www.x.com/rohxnrai" },
